9e47972249fe0d2684520f77a58b47f3f4f3a169
danicg <danicaldera@gmail.com>
2020-10-03 15:04:45 +0200

    feat: modify product scraper

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -45,6 +45,51 @@ const controller = {
             next(err);
         }
     },
+    getNutrimentsFromProducts: async ({query}, res, next) => {
+        const productsEan = query.ean && query.ean.length ? query.ean : [query.ean];
+        console.log(productsEan);
+        try {
+            const result = await Product.find({ean: {$in: productsEan}});
+            const nutriments = result.reduce(
+                (acum, prod) => {
+                    const product = prod._doc;
+                    return {
+                        carbohydrates100g:
+                            acum.carbohydrates100g +
+                            (product.nutriments.carbohydrates100g ? product.nutriments.carbohydrates100g : 0),
+                        energyKcal100g:
+                            acum.energyKcal100g +
+                            (product.nutriments.energyKcal100g ? product.nutriments.energyKcal100g : 0),
+                        fat100g: acum.fat100g + (product.nutriments.fat100g ? product.nutriments.fat100g : 0),
+                        nutritionDataPer: product.nutriments.nutritionDataPer,
+                        proteins100g:
+                            acum.proteins100g +
+                            (product.nutriments.proteins100g ? product.nutriments.proteins100g : 0),
+                        salt100g:
+                            acum.salt100g + (product.nutriments.salt100g ? product.nutriments.salt100g : 0),
+                        saturedFat100g:
+                            acum.saturedFat100g +
+                            (product.nutriments.saturedFat100g ? product.nutriments.saturedFat100g : 0),
+                        sugar100g:
+                            acum.sugar100g +
+                            (product.nutriments.sugar100g ? product.nutriments.sugar100g : 0),
+                    };
+                },
+                {
+                    carbohydrates100g: 0,
+                    energyKcal100g: 0,
+                    fat100g: 0,
+                    salt100g: 0,
+                    proteins100g: 0,
+                    saturedFat100g: 0,
+                    sugar100g: 0,
+                }
+            );
+            res.json(nutriments);
+        } catch (err) {
+            next(err);
+        }
+    },
 };
 
 module.exports = controller;
