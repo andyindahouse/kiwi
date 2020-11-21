2650aabdaf7dacf74158e14f30cbe5e978d1f1f2
danicg <danicaldera@gmail.com>
2020-11-21 13:12:49 +0100

    feat: unify shopping cart

--- backend/controllers/shoppingCart.js
+++ backend/controllers/shoppingCart.js
@@ -6,47 +6,47 @@ const errorTypes = require('./errorTypes');
 const {FEES} = require('../config');
 const utils = require('./utils');
 
+const calculateShoppingCart = async (shopppingCart) => {
+    if (shopppingCart && shopppingCart.products) {
+        const productsId = shopppingCart.products.map((product) => product.id);
+        const products = await Product.eci.find({id: {$in: productsId}});
+        let totalShoppingCart = 0;
+        const shoppingCartWithProducts = products.map((product) => {
+            const productInCart = shopppingCart.products.find((prodInCart) => prodInCart.id === product.id);
+            const costProduct = utils.getPrice(product._doc, productInCart.units);
+            totalShoppingCart = parseFloat((totalShoppingCart + costProduct).toFixed(2));
+            return {
+                ...product._doc,
+                units: productInCart.units,
+                note: productInCart.note,
+                cost: costProduct,
+            };
+        });
+        const totalCost = parseFloat((totalShoppingCart + FEES.deliverFee + FEES.shopperFee).toFixed(2));
+        return {
+            data: {
+                products: shoppingCartWithProducts,
+                totalShoppingCart,
+                deliverFee: FEES.deliverFee,
+                shopperFee: FEES.shopperFee,
+                totalCost,
+            },
+        };
+    } else {
+        return {
+            data: {
+                products: [],
+                totalCost: 0,
+            },
+        };
+    }
+};
 const controller = {
     get: async (req, res, next) => {
         try {
             const shopppingCart = await ShoppingCart.findOne({email: req.user.email});
-            if (shopppingCart && shopppingCart.products) {
-                const productsId = shopppingCart.products.map((product) => product.id);
-                const products = await Product.eci.find({id: {$in: productsId}});
-                let totalShoppingCart = 0;
-                const shoppingCartWithProducts = products.map((product) => {
-                    const productInCart = shopppingCart.products.find(
-                        (prodInCart) => prodInCart.id === product.id
-                    );
-                    const costProduct = utils.getPrice(product._doc, productInCart.units);
-                    totalShoppingCart = parseFloat((totalShoppingCart + costProduct).toFixed(2));
-                    return {
-                        ...product._doc,
-                        units: productInCart.units,
-                        note: productInCart.note,
-                        cost: costProduct,
-                    };
-                });
-                const totalCost = parseFloat(
-                    (totalShoppingCart + FEES.deliverFee + FEES.shopperFee).toFixed(2)
-                );
-                res.json({
-                    data: {
-                        products: shoppingCartWithProducts,
-                        totalShoppingCart,
-                        deliverFee: FEES.deliverFee,
-                        shopperFee: FEES.shopperFee,
-                        totalCost,
-                    },
-                });
-            } else {
-                res.json({
-                    data: {
-                        products: [],
-                        totalCost: 0,
-                    },
-                });
-            }
+            const data = await calculateShoppingCart(shopppingCart);
+            res.json(data);
         } catch (err) {
             next(err);
         }
@@ -65,38 +65,8 @@ const controller = {
                     useFindAndModify: true,
                 }
             );
-            if (shopppingCart && shopppingCart.products) {
-                const productsId = shopppingCart.products.map((product) => product.id).filter(Boolean);
-                const products = await Product.eci.find({id: {$in: productsId}});
-                let totalShoppingCart = 0;
-                const shoppingCartWithProducts = products.map((product) => {
-                    const productInCart = shopppingCart.products.find(
-                        (prodInCart) => prodInCart.id === product.id
-                    );
-                    const costProduct = utils.getPrice(product._doc, productInCart.units);
-                    totalShoppingCart = parseFloat((totalShoppingCart + costProduct).toFixed(2));
-                    return {
-                        ...product._doc,
-                        units: productInCart.units,
-                        note: productInCart.note,
-                        cost: costProduct,
-                    };
-                });
-                const totalCost = parseFloat(
-                    (totalShoppingCart + FEES.deliverFee + FEES.shopperFee).toFixed(2)
-                );
-                res.json({
-                    data: {
-                        products: shoppingCartWithProducts,
-                        totalShoppingCart,
-                        deliverFee: FEES.deliverFee,
-                        shopperFee: FEES.shopperFee,
-                        totalCost,
-                    },
-                });
-            } else {
-                res.json({data: []});
-            }
+            const data = await calculateShoppingCart(shopppingCart);
+            res.json(data);
         } catch (err) {
             next(err);
         }
