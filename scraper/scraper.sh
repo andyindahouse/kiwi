627ee2cb96b68e6d2e69410d4a7993b82be81b0f
danicg <danicaldera@gmail.com>
2020-12-08 12:30:01 +0100

    fix: script

--- scraper/scraper.sh
+++ scraper/scraper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # alimentacion desayunos lacteos congelados dieteticos bebidas frescos bebes higiene limpieza
-for section in bebes
+for section in alimentacion desayunos lacteos congelados dieteticos bebidas frescos bebes higiene limpieza
 do
     npm run list $section
 done
