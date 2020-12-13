780fe4d535cb4ae29ac50e5356df98cf5755128e
Daniel Caldera García <danicaldera@gmail.com>
2020-12-13 10:24:49 +0100

    Feat/new scraper list (#9)
    * feat: start scraper new
    
    * feat: working scraper
    
    * feat: working scraper
    
    * feat: add path server
    
    * fix: logs
    
    * fix: list
    
    * feat: working mongo
    
    * feat: working mongo
    
    * feat: working mongo
    
    * feat: select collection
    
    * feat: add script
    
    * feat: set no timeout
    
    * fix: script
    
    * checkpoint
    
    * add api urls
    
    * feat: discomment mongo
    
    * fix: config
    
    * feat: remove logs
    
    * fix: comment
    
    Co-authored-by: Anderson Méndez Abello <amendez@tuenti.com>

--- scraper/list-scraper.js
+++ scraper/list-scraper.js
@@ -5,118 +5,176 @@ const config = require('./config');
 
 puppeteer.use(StealthPlugin());
 
-const getTotalPages = async (page, url) => {
-    await page.goto(url);
-    await page.waitForTimeout(config.timeout);
-    const pages = await page.evaluate(() =>
-        document.querySelector('.pagination').getAttribute('data-pagination-total')
-    );
-    return pages;
+const getTotalProducts = async (page) => {
+    const title = await page.evaluate(() => document.title);
+    console.log(title);
+    const products = title.split('(')[1].substring(0, title.split('(')[1].length - 1);
+    return products;
 };
 
-const getProductsPage = async (page, url) => {
-    const getProducts = async (url) => {
-        await page.goto(url);
-        await page.waitForTimeout(config.timeout);
-        const data = await page.evaluate(() => {
-            const getDiscountType = (specialOffer) => {
-                if (/(\d+).* unidad al (\d+).* de descuento/.test(specialOffer)) {
-                    const [text, valueOne, valueTwo] = /(\d+).* unidad al (\d+).* de descuento/.exec(
-                        specialOffer
-                    );
+const getProducts = async (page) => {
+    const data = await page.evaluate(() => {
+        const getDiscountType = (specialOffer) => {
+            if (/(\d+).* unidad al (\d+).* de descuento/.test(specialOffer)) {
+                const [text, valueOne, valueTwo] = /(\d+).* unidad al (\d+).* de descuento/.exec(
+                    specialOffer
+                );
+                return {
+                    specialOffer: 'offerDiscount',
+                    specialOfferValue: [valueOne, valueTwo],
+                };
+            }
+            if (/Lleva (\d+) y paga (\d+)/.test(specialOffer)) {
+                const [text, valueOne, valueTwo] = /Lleva (\d+) y paga (\d+)/.exec(specialOffer);
+                return {
+                    specialOffer: 'quantityDiscount',
+                    specialOfferValue: [valueOne, valueTwo],
+                };
+            }
+        };
+        return [...document.querySelectorAll('.grid-item')]
+            .filter((elem) => !!elem)
+            .map((elem) => {
+                const jsonData = elem.getAttribute('data-json');
+                const offerData = elem.querySelector('.offer-description');
+                if (jsonData) {
                     return {
-                        specialOffer: 'offerDiscount',
-                        specialOfferValue: [valueOne, valueTwo],
+                        ...JSON.parse(elem.getAttribute('data-json')),
+                        img: 'https:' + elem.querySelector(' * > img').getAttribute('src'),
+                        url: elem.querySelector('a').getAttribute('href'),
+                        ...(offerData
+                            ? {...getDiscountType(offerData.textContent)}
+                            : {specialOffer: null, specialOfferValue: null}),
                     };
                 }
-                if (/Lleva (\d+) y paga (\d+)/.test(specialOffer)) {
-                    const [text, valueOne, valueTwo] = /Lleva (\d+) y paga (\d+)/.exec(specialOffer);
-                    return {
-                        specialOffer: 'quantityDiscount',
-                        specialOfferValue: [valueOne, valueTwo],
-                    };
-                }
-            };
-            return [...document.querySelectorAll('.grid-item')]
-                .filter((elem) => !!elem)
-                .map((elem) => {
-                    const jsonData = elem.getAttribute('data-json');
-                    const offerData = elem.querySelector('.offer-description');
-                    if (jsonData) {
-                        return {
-                            ...JSON.parse(elem.getAttribute('data-json')),
-                            img: 'https:' + elem.querySelector(' * > img').getAttribute('src'),
-                            url: elem.querySelector('a').getAttribute('href'),
-                            ...(offerData
-                                ? {...getDiscountType(offerData.textContent)}
-                                : {specialOffer: null, specialOfferValue: null}),
-                        };
-                    }
-                    return null;
-                })
-                .filter(Boolean);
-        });
-        return data;
-    };
-    const products = await getProducts(url);
-    return products;
+                return null;
+            })
+            .filter(Boolean);
+    });
+    return data;
+};
+
+const scrapeInfiniteScrollItems = async (page, itemTargetCount, scrollDelay = 1000) => {
+    let items = 0;
+    try {
+        let previousHeight;
+        while (items < itemTargetCount) {
+            items = await page.evaluate(() => {
+                return [...document.querySelectorAll('.grid-item')].length;
+            });
+            if (items < itemTargetCount) {
+                previousHeight = await page.evaluate('document.body.scrollHeight');
+                await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+                await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+                await page.waitForTimeout(scrollDelay);
+            }
+            console.log(items, itemTargetCount);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    return await getProducts(page);
 };
 
 (async () => {
     console.log('Starting scraping list...');
     console.log(new Date());
-    console.time('Scraping');
-    const urls = config.scrapingUrl;
+    console.time('Scraping time');
+    let urls = config.scrapingUrls;
+    if (process.argv.length === 3) {
+        urls = [urls.find((url) => url.name === process.argv[2])];
+        if (!urls) {
+            console.log(`Section don't exist`);
+            process.exit(404);
+        }
+    }
     const browser = await puppeteer.launch(config.pupetterOptions);
     const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    for (let i = 0; i <= urls.length; i++) {
-        const url = urls[i].url;
-        const pages = await getTotalPages(page, url);
-        console.log(`Scraping category ${url}...`);
-        let urlWithPage = `${url}`;
-        let client;
+
+    for (let i = 0; i < urls.length; i++) {
         try {
+            const supermarket = urls[i];
+            const url = urls[i].url;
+            console.log(`Scraping category ${url}...`);
+            await page.goto(url, {waitUntil: 'networkidle0'});
+            await page.waitForTimeout(1000); // redirect from check page
+
+            const products = await getTotalProducts(page);
+            console.log(products);
+            let urlWithPage = `${url}`;
+            let client;
+
+            const paginationEnabled = await page.evaluate(
+                `document.querySelector('.button._pagination._on')`
+            );
+            console.log(paginationEnabled);
+            if (!paginationEnabled) {
+                console.log('enable infinite scroll...');
+
+                //Enable infinite scroll
+                const searchButtonNodeSelector = '.button._pagination';
+                await page.click(searchButtonNodeSelector);
+                // In this point if the checks to avoid scrapping
+                await page.waitForTimeout(1000); // refresh when pagination mode change
+            }
+
+            //Polling
+            await page.evaluate('window.scrollBy(0, 100000000)');
+            let numberPage = 2;
+            let pageSize = 24;
+            console.time('pagination');
+            while (numberPage * pageSize < products) {
+                console.log(numberPage * pageSize, products);
+                await page.waitForResponse(supermarket.api.replace('%NUMBERPAGE%', numberPage), {
+                    timeout: 9999,
+                });
+                numberPage += 1;
+            }
+            console.timeEnd('pagination');
+
+            const prods = await scrapeInfiniteScrollItems(page, parseInt(products));
+            console.log('scraped', prods.length);
+            console.log('Inserting items...');
             client = await mongodb.MongoClient.connect(config.configMongo.url, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             });
             const collection = await client.db(config.configMongo.db).collection(urls[i].collection);
-            for (let j = 1; j <= pages; j++) {
-                urlWithPage = `${url}${j}`;
-                const prods = await getProductsPage(page, urlWithPage);
-                prods.forEach(async (product) => {
-                    const productData = {
-                        ...product,
-                        market: config.collectionProducts[config.indexCollection].market,
-                        img: product.img.replace('40x40.', '325x325.'),
-                        updateDate: new Date(),
-                    };
-                    await collection.findOne({id: productData.id});
-                    const result = await collection.updateOne(
-                        {id: productData.id},
-                        {
-                            $set: {...productData},
-                        }
-                    );
-                    if (result.matchedCount === 0) {
-                        await collection.insertOne({
-                            ...productData,
-                            createDate: new Date(),
-                            available: true,
-                        });
+
+            prods.forEach(async (product) => {
+                const productData = {
+                    ...product,
+                    market: config.collectionProducts[config.indexCollection].market,
+                    img: product.img.replace('40x40.', '325x325.'),
+                    updateDate: new Date(),
+                };
+                await collection.findOne({id: productData.id});
+                const result = await collection.updateOne(
+                    {id: productData.id},
+                    {
+                        $set: {...productData},
                     }
-                });
-                console.log(`${pages - j} pages left.`);
-            }
-            await browser.close();
-            console.log(`Scraper finished.`);
-            console.log(new Date());
-            console.time('Scraping');
+                );
+                if (result.matchedCount === 0) {
+                    await collection.insertOne({
+                        ...productData,
+                        createDate: new Date(),
+                        available: true,
+                    });
+                }
+            });
         } catch (e) {
             console.log(e);
-        } finally {
-            client.close();
         }
+        console.log('Items inserted.');
     }
+    try {
+        await browser.close();
+    } catch (e) {
+        console.log(e);
+    }
+    console.log(`Scraper finished.`);
+    console.log(new Date());
+    console.timeEnd('Scraping time');
+    process.exit();
 })();
---
+++ scraper/scraper.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# alimentacion desayunos lacteos congelados dieteticos bebidas frescos bebes higiene limpieza
+for section in alimentacion desayunos lacteos congelados dieteticos bebidas frescos bebes higiene limpieza
+do
+    npm run list $section
+done
