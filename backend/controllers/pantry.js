c4b903b1a5b0aa905a42bd58514743eec0524836
danicg <danicaldera@gmail.com>
2021-02-06 13:57:57 +0100

    feat: order first date in pantry

--- backend/controllers/pantry.js
+++ backend/controllers/pantry.js
@@ -26,7 +26,14 @@ const controller = {
             };
             try {
                 const totalSize = await Pantry.find(findPantryProducts).countDocuments();
-                const result = await Pantry.find(findPantryProducts).sort(order).skip(skip).limit(limit);
+                const result = await Pantry.aggregate([
+                    {$match: findPantryProducts},
+                    {$addFields: {fieldType: {$type: '$date'}}},
+                    {$sort: {fieldType: 1, ...order}},
+                    {$project: {fieldType: 0}},
+                    {$skip: skip},
+                    {$limit: limit},
+                ]);
                 res.json({
                     pageNumber,
                     pageSize,
