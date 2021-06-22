d86a653f4ecd2e1111b0335c24aff6eb80406cd5
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2021-06-22 22:49:54 +0200

    Upgrade to Yarn 2 & workspaces

--- scraper/config.js
+++ scraper/config.js
@@ -20,71 +20,61 @@ const scrapingUrls = [
     {
         name: 'alimentacion',
         url: `${marketUrl}/supermercado/alimentacion-general/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/alimentacion-general/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/alimentacion-general/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'desayunos',
         url: `${marketUrl}/supermercado/desayunos-dulces-y-pan/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/desayunos-dulces-y-pan/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/desayunos-dulces-y-pan/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'lacteos',
         url: `${marketUrl}/supermercado/lacteos/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/lacteos/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/lacteos/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'congelados',
         url: `${marketUrl}/supermercado/congelados/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/congelados/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/congelados/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'dieteticos',
         url: `${marketUrl}/supermercado/dieteticos/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/dieteticos/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/dieteticos/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'bebidas',
         url: `${marketUrl}/supermercado/bebidas/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/bebidas/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/bebidas/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'frescos',
         url: `${marketUrl}/supermercado/frescos/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/frescos/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/frescos/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'bebes',
         url: `${marketUrl}/supermercado/bebes/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/bebes/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/bebes/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'higiene',
         url: `${marketUrl}/supermercado/cuidado-e-higiene-personal/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/cuidado-e-higiene-personal/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/cuidado-e-higiene-personal/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
     {
         name: 'limpieza',
         url: `${marketUrl}/supermercado/drogueria-y-limpieza/`,
-        api:
-            'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/drogueria-y-limpieza/%NUMBERPAGE%/?direction=next',
+        api: 'https://www.elcorteingles.es/alimentacion/api/catalog/010MOE/get-page/supermercado/drogueria-y-limpieza/%NUMBERPAGE%/?direction=next',
         collection: collectionProducts[indexCollection].collection,
     },
 ];
