da61021711edbcc79b3033d1b2e8bcd4ee12bcb4
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2021-06-26 11:51:39 +0200

    Share API client

---
+++ packages/api/src/index.ts
@@ -0,0 +1,32 @@
+export const getApiCall =
+    (host: string, getAuthorizationBearer: () => string | null) =>
+    async (
+        {
+            url,
+            body,
+            customMethod,
+        }: {url: string; body?: Record<string, any> | FormData; customMethod?: string},
+        authenticated = true
+    ) => {
+        const method = customMethod || (body ? 'POST' : 'GET');
+        const authorizationBearer = getAuthorizationBearer();
+        if (authenticated && !authorizationBearer) {
+            throw new Error('No authorization found');
+        }
+
+        const rawResponse = await fetch(`${host}/api${url}`, {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                ...(authenticated ? {Authorization: `Bearer ${authorizationBearer}`} : {}),
+            },
+            ...(body ? {body: JSON.stringify(body)} : {}),
+        });
+
+        if (!rawResponse.ok) {
+            throw Error(`ERROR API ${method} RESPONSE ${url}: ${rawResponse.status}`);
+        }
+
+        const response = await rawResponse.json();
+        return response.data || response;
+    };
