a086aa57db025d1bab389598a7f739b5a5165204
Anderson Méndez Abello <amendez@tuenti.com>
2021-06-28 21:26:16 +0200

    rename const

--- backend/controllers/utils.js
+++ backend/controllers/utils.js
@@ -1,23 +1,23 @@
 import {FEES} from '../config.js';
 
 export const getPrice = (product, units) => {
-    const priceFinal = Number(product.price.final);
+    const finalPrice = Number(product.price.final);
 
     if (product.specialOffer === 'offerDiscount') {
         const quotient = Math.floor(units / product.specialOfferValue[0]);
-        const discount = quotient * ((priceFinal * (100 - product.specialOfferValue[1])) / 100);
+        const discount = quotient * ((finalPrice * (100 - product.specialOfferValue[1])) / 100);
         const withoutDiscount = units - quotient;
-        return parseFloat((withoutDiscount * priceFinal + discount).toFixed(2));
+        return parseFloat((withoutDiscount * finalPrice + discount).toFixed(2));
     } else if (product.specialOffer === 'quantityDiscount') {
-        const value = priceFinal;
+        const value = finalPrice;
         const rest = units % product.specialOfferValue[0];
         const quotient = Math.floor(units / product.specialOfferValue[0]);
         const productsQuantity = quotient * product.specialOfferValue[1] + rest;
         return parseFloat((productsQuantity * value).toFixed(2));
     } else if (product.saleType === 'unit') {
-        return parseFloat((units * priceFinal).toFixed(2));
+        return parseFloat((units * finalPrice).toFixed(2));
     } else {
-        const priceFor100gr = priceFinal / 10;
+        const priceFor100gr = finalPrice / 10;
         const unitsOf100gr = units / 100;
         return parseFloat((unitsOf100gr * priceFor100gr).toFixed(2));
     }
